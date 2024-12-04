4b2995c700a911a01c82ae22b731aa3f0e68d477
Victor Redondo <vrviktor@gmail.com>
2024-12-04 10:20:23 +0100

    Exercise 3 level 1 done

---
+++ js/Person.js
@@ -0,0 +1,7 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  greet = () => console.log(`Hola, ${this.name}`);
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 const add = (a, b) => a + b;
 
 const randomNumber = () => Math.floor(Math.random() * 101);
+
+const person = new Person("Isma");
+
+person.greet();
