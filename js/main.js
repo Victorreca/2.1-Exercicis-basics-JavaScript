0573a64172c46f441322db1f22b15a3b7eee2d3b
Victor Redondo <vrviktor@gmail.com>
2024-12-04 10:43:36 +0100

    Exercise 4 nivell 2 done

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ const randomNumber = () => Math.floor(Math.random() * 101);
 const person = new Person("Isma");
 
 person.greet();
+
+const arrayNumbers = [0, 2, 5];
+
+const printNumbers = (arrayNumbersValue) => {
+  for (let i = 0; i < arrayNumbersValue.length; i++) {
+    (() => console.log(arrayNumbers[i]))();
+  }
+};
+printNumbers(arrayNumbers);
