844aa429aafb680e06b75153363b37e6ab4bca4a
Victor Redondo <vrviktor@gmail.com>
2024-12-05 10:22:18 +0100

    Do exercise 1.4 Rest & Spread operators level 1 exercise 1

--- js/main.js
+++ js/main.js
@@ -117,3 +117,8 @@ const capitalLetters = (wordToCapitalLetters) =>
 const wordToChange = "hola";
 
 console.log(processarCadena(wordToChange, capitalLetters));
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const array3 = [...array1, ...array2];
+console.log(array3);
