1089a0f23a30cf06c63318787242d670b60ec6e7
Victor Redondo <vrviktor@gmail.com>
2024-12-05 10:29:23 +0100

    Do exercise 1.4 Rest & Spread operators level 1 exercise 2

--- js/main.js
+++ js/main.js
@@ -122,3 +122,13 @@ const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 const array3 = [...array1, ...array2];
 console.log(array3);
+
+const suma = (...numbers) => {
+  let result = 0;
+  numbers.forEach((number) => {
+    result += number;
+  });
+  return result;
+};
+
+console.log(suma(3, 4, 2));
