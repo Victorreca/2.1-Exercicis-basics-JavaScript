2584d9ab2c7039dfab00f46b0ccf05c8aac501f1
Victor Redondo <vrviktor@gmail.com>
2024-12-07 13:00:46 +0100

    Refactor exercise 3

--- js/main.js
+++ js/main.js
@@ -260,26 +260,16 @@ promiseHelloWorld.then((message) => {
 const promiseResolveReject = (input) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (input === "Hola") {
-        resolve("Promesa resolta");
-      } else {
-        reject("Promesa rebutjada");
-      }
+      input === "Hola"
+        ? resolve("Promesa resolta")
+        : reject("Promesa rebutjada");
     }, 2000);
   });
 
 promiseResolveReject("Hola")
-  .then((exit) => {
-    console.log(exit);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then((exit) => console.log(exit))
+  .catch((error) => console.log(error));
 
 promiseResolveReject("Adéu")
-  .then((exit) => {
-    console.log(exit);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then((exit) => console.log(exit))
+  .catch((error) => console.log(error));
