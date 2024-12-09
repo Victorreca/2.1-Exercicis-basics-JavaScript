06a96b0f0ee5d36a81501b4e9c952c37f1248b26
Victor Redondo <vrviktor@gmail.com>
2024-12-09 11:05:53 +0100

    Move exercise 1.1 level 1 files

---
+++ js/1-1-arrow-functions-level-1.js
@@ -0,0 +1,11 @@
+// Exercici 1
+const add = (a, b) => a + b;
+console.log(`Function suma: ${add(2, 4)}`);
+
+// Exercici 2
+const randomNumber = () => Math.floor(Math.random() * 101);
+console.log(`Function número aleatori: ${randomNumber()}`);
+
+// Exercici 3
+const person = new Person("Isma");
+person.greet();
--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-const add = (a, b) => a + b;
-
-const randomNumber = () => Math.floor(Math.random() * 101);
-
-const person = new Person("Isma");
-
-person.greet();
-
 const arrayNumbers = [0, 2, 5];
 
 const printNumbers = (arrayNumbersValue) => {
